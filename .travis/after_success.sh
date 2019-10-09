5337ad0f9c8d1c1ae995d04105547e8d198e1724
Ivan De Marino <detronizator@gmail.com>
2019-10-09 20:57:11 +0200

    Try again

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -3,8 +3,9 @@
 IMAGE_NAME=$1
 echo "Will build image with name: ${IMAGE_NAME}"
 
+cargo pkgid
 IMAGE_VERSION=$(cargo pkgid | grep -o "\d.\d.\d$")
-echo "Will build image with version ${IMAGE_VERSION}"
+echo "Will build image with version: ${IMAGE_VERSION}"
 
 docker build \
   --pull --cache-from "${IMAGE_NAME}" \
